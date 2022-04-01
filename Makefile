8167833c8a77a95c0edf930b9a87507bcc1570f4
Vinson Ong <vinson1493@gmail.com>
2022-04-01 15:38:33 +0800

    Added normalization of email

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 test:
 	docker-compose run app sh -c "python manage.py test"
+
+migrate-core:
+	docker-compose run app sh -c "python manage.py makemigrations core"
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+  mock_email = "vinson@vinson.sg"
+  mock_password = "p@$$w0rd12345"
+
+  def test_create_user_with_email_successful(self):
+    """Test creating a new user with mock email is successful"""
+    user = get_user_model().objects.create_user(
+        email=self.mock_email,
+        password=self.mock_password
+    )
+
+    self.assertEqual(user.email, self.mock_email)
+    self.assertTrue(user.check_password(self.mock_password))
+
+  def test_new_user_email_normalized(self):
+    email = 'vinson@vInsON.Sg'
+    user = get_user_model().objects.create_user(
+        email=email,
+        password=self.mock_password
+    )
+
+    self.assertEqual(user.email, email.lower())
