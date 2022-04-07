096f9effe6950fb608f3867a84ce706082211aba
Vinson Ong <vinson1493@gmail.com>
2022-04-08 03:03:02 +0800

    58. added ingredient model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -53,3 +53,9 @@ class ModelTests(TestCase):
     """Test the tag string representation"""
     tag = models.Tag.objects.create(user=sample_user(), name='Vegan')
     self.assertEqual(str(tag), tag.name)
+
+  def test_ingredient_str(self):
+    """Test the ingredient string representation"""
+    ingredient = models.Ingredient.objects.create(
+        user=sample_user(), name='Cucumber')
+    self.assertEqual(str(ingredient), ingredient.name)
